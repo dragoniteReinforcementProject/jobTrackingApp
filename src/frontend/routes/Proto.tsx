c63c069af42c849fa2e6f84f07e43602d9927043
Evan Pearson <parsnbl@icloud.com>
2023-12-19 19:36:02 -0800

    finished the Modal component, ready for hooks/integration

--- src/frontend/routes/Proto.tsx
+++ src/frontend/routes/Proto.tsx
@@ -1,21 +1,52 @@
-import React from 'react';
-import style from '../styles/Proto.module.scss';
-import Google from '../assets/google.png';
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import React, { useState } from 'react';
+import styles from '../styles/Proto.module.scss'
+import type { ChangeEventHandler } from 'react';
+import { format } from 'date-fns';
+import { Save, X } from 'lucide-react';
 
+import JobModal from '../components/JobModal';
+import JobSlug from '../components/JobSlug';
+
+const sentenceCase = (str: string): string => {
+  let output = str.replaceAll(/[A-Z]/g, char => ' ' + char);
+  output = output.slice(0, 1).toUpperCase() + output.slice(1);
+  return output;
+};
+const tagCase = (str: string): string => {
+  return str.replaceAll(/[A-Z]/g, char =>
+    '-' + char.toLowerCase())
+}
+
+type _JobStatus = 'Wishlist' | 'Applied' | 'Phone' | 'Interview' | 'Offer' | 'Rejected';
+
+enum JobStatus {
+  wishlist = 'Wishlist',
+  applied = 'Applied',
+  phone = 'Phone',
+  interview = 'Interview',
+  offer = 'Offer',
+  rejected = 'Rejected'
+}
+
+const obj: CompanyProps = {
+  jobTitle: 'Backend Developer Extra cool mega props for my guy',
+  company: 'Google',
+  dateAdded: new Date(2023, 11, 1),
+  link: 'https://www.google.com/',
+  color: 2,
+  deadline: new Date(2024, 0, 30),
+  location: 'Cupertino, CA',
+  salary: '$200,000',
+  jobStatus: JobStatus.offer,
+  notes: ''
+}
 
 const Proto = (): JSX.Element => {
+ 
+
   return (
-    <div>
-      <div className={style.jobContainer}>
-        <div className={style.jobIcon}>
-          <img src={Google}/>
-        </div>
-        <div className={style.jobText}>
-          <p style={{ fontStyle: 'italic'}}>Senior Frontend Engineer, II</p>
-          <p style={{ fontWeight: 'bolder' }}>GOOGLE</p>
-        </div>
-      </div>
-    </div>
+   <JobModal {...obj} />
   );
 };
 
